d0d6aaa12adf396641aa436ddde92b05454078cd
sreyaku <90330114+sreyaku@users.noreply.github.com>
2022-12-12 15:40:37 +0530

    Update upload.groovy

--- vars/upload.groovy
+++ vars/upload.groovy
@@ -1,6 +1,6 @@
 def call(){
   echo "Uploading the artifact to S3 ..."
-  bat '''
-  aws s3 cp ${WORKSPACE}/target/*.war s3://devopslearning-jenkinsharedlibrary
-  '''
+//   bat '''
+//   aws s3 cp ${WORKSPACE}/target/*.war s3://devopslearning-jenkinsharedlibrary
+//   '''
 }
